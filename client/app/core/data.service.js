54513748d95414b52520c2f765776ef8cfbbb6e6
makidelille <makidelille@hotmail.fr>
2018-07-29 17:00:07 +0200

    add css and test

--- client/app/core/data.service.js
+++ client/app/core/data.service.js
@@ -2,17 +2,89 @@
 angular.module("statsApp.core")
     .factory("dataService", dataServiceFactory)
 
-dataServiceFactory.$inject= ['$http']
-function dataServiceFactory($http){
+dataServiceFactory.$inject= ['$http', '$q']
+function dataServiceFactory($http, $q){
+
+    function processData(data){
+        return $q(function(resolve, reject){
+            var clubs = data.map(function(club){
+                // calculate club stats
+                var h = 0;
+                var f = 0;
+                var ageMoyen = 0;
+                var ageMin = Infinity;
+                var ageMax = -Infinity;
+                var ages = {};
+                
+                club.membres.forEach(function(membre){
+                    switch(membre.sexe){
+                        case "M": 
+                        h++;
+                        break;
+                        case "F":
+                        f++;
+                        break;
+                    }
+                    
+                    var age = membre.age;
+                    if(age === null || age === undefined){
+                        return;
+                    }
+                    
+                    if(ages[age] === undefined){
+                        ages[age] = 0;
+                    }
+                    
+                    if(age < ageMin){
+                        ageMin = age;
+                    }
+                    
+                    if(age > ageMax){
+                        ageMax = age;
+                    }
+                    ages[age]++;
+                    ageMoyen += age;
+                });
+                
+                var parite = h / club.membres.length;
+                var ageMoyen = ageMoyen / club.membres.length;
+                
+                club.stats = {
+                    parite: parite,
+                    mCount: h,
+                    fCount: f,
+                    ageMoyen: ageMoyen,
+                    ageMin: Math.max(ageMin, 18),
+                    ageMax: ageMax,
+                    ages: ages
+                };
+                
+                return club;
+                });
+            return resolve(clubs);
+        });
+    }
+
     
     var getExample = function(){
-        return $http.get('/data/example.json', {
-        
+        return $http.get('/data/example.json', {})
+        .then(function(response){
+            // stats caluclation
+            return processData(response.data);
         });
+    }
+
+    var getLatest = function(){
+        return $http.get('/data/latest.json', {})
+            .then(function(response){
+                // stats caluclation
+                return processData(response.data);
+            });
     }
         
 
     return {
-        getExample: getExample
+        getExample: getExample,
+        getLatest: getLatest
     }
 }
--- client/app/global/global.js
+++ client/app/global/global.js
@@ -9,76 +9,90 @@ angular.module("statsApp.global", ["statsApp.core"])
 })
 .controller("globalCtrl", globalCtrl)
 
-globalCtrl.$inject = ["$scope", "dataService"];
-function globalCtrl($scope, dataService){
+globalCtrl.$inject = ["$scope", "$q", "dataService"];
+function globalCtrl($scope, $q, dataService){
     $scope.initialized = false;
-    $scope.data = [];
 
-    $scope.hommeFemme = {};
-    $scope.ages = {};
+    $scope.hommeFemme = {
+        avg: 0,
+        data: [0,0],
+        labels: ["Homme", "Femme"]
+    };
+    $scope.membresCount = 0;
+    $scope.ages = {
+        max:-Infinity,
+        min:+Infinity,
+        avg:0,
+        data: [],
+        labels: [],
+    };
 
-    dataService.getExample().then(function(response){
-        $scope.data = response.data;
-        console.log(response.data);
-        
-        calcultateHommeFemme();  
-        calcultateAge();      
-        
+    dataService.getLatest().then(function(data){
+        return concat(data);
+    }).then(function(scope){
+        console.log(scope);
+        $scope.hommeFemme = scope.hommeFemme;
+        $scope.ages = scope.ages;
+        $scope.membresCount = scope.membresCount;
         $scope.initialized = true;
     });
 
-    function calcultateHommeFemme(){
-        var h = 0;
-        var f = 0;
-
-        $scope.data.forEach(function(element) {
-            element.membres.forEach(function(ele){
-                switch(ele.sexe){
-                    case "M": 
-                        h++;
-                        break;
-                    case "F":
-                        f++;
-                        break;
+    //filter before function
+    function concat(data){ 
+        return $q(function(resolve, reject){
+            var scope = {
+                membresCount:0,
+                hommeFemme:{
+                    data:[0,0],
+                    labels:["Homme", "Femme"]
+                },
+                ages:{
+                    max:10,
+                    min:+120,
+                    avg:0,
+                    data: [],
+                    labels: [],
                 }
-            })
-        });
-
-
-        $scope.hommeFemme = {
-            data: [h, f],
-            labels: ["homme", "femme"]
-        }
-
-    }
-
-    function calcultateAge(){
-        var obj = {}
-
-        $scope.data.forEach(function(element) {
-            element.membres.forEach(function(ele){
-                var age = ele.age;
-                if(obj[age] === undefined){
-                    obj[age] = 0;
-                }
-                obj[age]++;
-            })
-        });
-
-
-        $scope.ages = {
-            data: [],
-            labels: []
-        }  
-
-        for(var age in obj){
-            if(age === "null" || age === "undefined") {
-                continue
             };
-            $scope.ages.data.push(obj[age]);
-            $scope.ages.labels.push(age + " ans");
-        };
 
+            var temp = [];
+            data.forEach(element => {
+                var stats = element.stats;
+                
+                scope.membresCount += element.membres.length;
+                scope.hommeFemme.data[0] += stats.mCount;
+                scope.hommeFemme.data[1] += stats.fCount;
+                
+                if(stats.ageMax > scope.ages.max){
+                    scope.ages.max = stats.ageMax;
+                }
+                
+                if(stats.ageMin < scope.ages.min){
+                    scope.ages.min = stats.ageMin;
+                }
+                
+                temp.push(stats.ages);
+            });
+            
+            //ages assign
+            var total=0;
+            for(var age=scope.ages.min; age <= scope.ages.max; age++){
+                var sum = 0;
+                for (var i = 0; i < temp.length; i++) {
+                    if(temp[i][age]){
+                        var count = temp[i][age];
+                        sum += count;
+                        total += age * count;
+                    }
+                }
+                scope.ages.data.push(sum);
+                scope.ages.labels.push(age + " ans");
+            }
+
+            scope.ages.avg = total/scope.membresCount;
+            scope.hommeFemme.avg = scope.hommeFemme.data[0]/scope.hommeFemme.data[1];
+            return resolve(scope);
+        });
     }
 
    
